1766715fa71fe6d1eb4eaa959ba4bb6b4948b35a
Zachary Ozer <zach@theozer.com>
2012-11-25 21:53:11 -0500

    Initial import

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<?php if(!isset($title)) {
+$title = 'zachary adam ozer';
+}
+?>
+<title>the ozer - <?php echo $title; ?></title>
+<link rel="stylesheet" type="text/css" href="/css/screen-style.css" />
+<meta name="google-site-verification" content="wq5y2oQmkjnJIDRE-ShGB2GnloSTnLOLKiI-8lX3lso" />
+</head>
+<body>
+<div id="main">
+<div class="logo"><a href="/">&lt;theozer.com&gt;</a></div>
+<div id="splash">
+<div id="name"><?php echo $title; ?></div>
+<div id="navbar">
+<ul>
+<li><a href="/portfolio/">portfolio</a></li>
+<li><a href="http://zacharyozer.blogspot.com/">blog</a></li>
+<li><a href="mailto:contact@theozer.com?subject=Comments from theozer.com">contact</a></li>
+<li><a href="/misc/resume.pdf">résumé</a></li>
+</ul>
+</div>
+<hr />
---
+++ index.php
@@ -0,0 +1,2 @@
+<?php require $_SERVER['DOCUMENT_ROOT'].'/header.php';?>
+<?php require $_SERVER['DOCUMENT_ROOT'].'/footer.php';?>
